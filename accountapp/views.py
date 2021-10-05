7c12f117c3d2e0569b07aae14cfcac7f67e0e83d
meongj <89771235+meongj@users.noreply.github.com>
2021-10-05 17:49:08 +0900

    django course 24 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from accountapp.models import HelloWorld
 
@@ -44,3 +44,9 @@ class AccountCreateView(CreateView):
     success_url = reverse_lazy('accountapp:hello_world')
     # 어느 app에서 볼껀지 설정, 사용자화면단
     template_name = 'accountapp/create.html'
+
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
