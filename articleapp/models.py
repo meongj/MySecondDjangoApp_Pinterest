f99b190b0fa8d2d33d1dce62f1b2e593ac6567af
meongj <89771235+meongj@users.noreply.github.com>
2021-10-10 21:03:32 +0900

    django course 35+a commit

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    # SET_NULL : User 탈퇴시에도 게시글은 삭제되지 않음 (알수없음)
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)
+    title = models.CharField(max_length=200, null=True)
+    # 이미지는 article/ 경로 밑에 저장
+    # 이미지는 항상 넣도록 하기
+    image = models.ImageField(upload_to='article/', null=False)
+    content = models.TextField(null=True)
+    # 생성됬을 때 자동으로 생성시간 저장됨
+    created_at = models.DateField(auto_now_add=True, null=True)
--- articleapp/urls.py
+++ articleapp/urls.py
@@ -2,8 +2,17 @@ from django.urls import path
 
 from django.views.generic import TemplateView
 
+from articleapp.views import ArticleCreateView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView
+
+app_name = 'articleapp'
+
 urlpatterns = [
     # 장고지정 TemplateView
     # 템플릿만 지정해주면 알아서 만들어줌
     path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list'),
+    path('create/', ArticleCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),
+    path('update/<int:pk>', ArticleUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', ArticleDeleteView.as_view(), name='delete'),
+
 ]
