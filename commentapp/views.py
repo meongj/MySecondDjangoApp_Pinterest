3c46a8c8e8f531ec667555fbcf03a24e478f4c69
meongj <89771235+meongj@users.noreply.github.com>
2021-10-12 15:27:14 +0900

    django course 38 commit

--- commentapp/views.py
+++ commentapp/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, DeleteView
 
 from articleapp.models import Article
+from commentapp.decorators import comment_ownership_required
 from commentapp.forms import CommentCreationForm
 from commentapp.models import Comment
 
@@ -22,6 +24,18 @@ class CommentCreateView(CreateView):
         temp_comment.writer = self.request.user
         temp_comment.save()
         return super().form_valid(form)
+
+    def get_success_url(self):
+        # 댓글 작성후 성공시 작성한 detail페이지로 다시 돌아옴
+        return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
+
+
+@method_decorator(comment_ownership_required, 'get')
+@method_decorator(comment_ownership_required, 'post')
+class CommentDeleteView(DeleteView):
+    model = Comment
+    context_object_name = 'target_comment'
+    template_name = 'commentapp/delete.html'
 
     def get_success_url(self):
         # 댓글 작성후 성공시 작성한 detail페이지로 다시 돌아옴
